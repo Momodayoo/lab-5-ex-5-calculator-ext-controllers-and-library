aaf578802ee9c83411619c0c26714ba23f59dd16
Momodayoo <msyauqi94@gmail.com>
2023-11-11 15:25:36 +0800

    swagger made

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+const swaggerUi = require('swagger-ui-express');
+swaggerDocument = require('./swagger.json');
+
+
+
 const Calculator = require('./libraries/Calculator');
 let myCalc1 = new Calculator()
 let myCalc2 = new Calculator()
-myCalc1.add(3,4)
+myCalc1.add(3,44)
 myCalc1.add(4,2)
-myCalc2.add(5,4)
+myCalc2.add(66,4)
 
 
 const calculatorRoutes = require('./routes/calculatorRoutes');
@@ -19,6 +24,9 @@ app.use ('/', express.static('public'));
 
 app.use('/mytest', testRoutes);
 app.use('/calculator', calculatorRoutes);
+
+app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerDocument)
+    );
 
 app.get('/', (req, res) => {
     res.send ("Hellow World!");
