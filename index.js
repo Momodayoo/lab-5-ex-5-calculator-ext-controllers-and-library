40d9e5667ed479c6caf25c9862ac0742da5431ab
Momodayoo <msyauqi94@gmail.com>
2023-11-11 09:15:54 +0800

    commit on main

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const Calculator = require('./libraries/Calculator');
+let myCalc1 = new Calculator()
+let myCalc2 = new Calculator()
+myCalc1.add(3,4)
+myCalc1.add(4,2)
+myCalc2.add(5,4)
+
 
 const calculatorRoutes = require('./routes/calculatorRoutes');
 
--- routes/calculatorRoutes.js
+++ routes/calculatorRoutes.js
@@ -1,40 +1,21 @@
 const express = require('express');
+const calculatorController = require ('../controller/calculatorController')
 const router = express.Router();
 
 router.get('/add', (req, res) => {
-    let number1 = parseInt(req.query.num1);
-    let number2 = parseInt(req.query.num2);
-    let sum = number1 + number2
-    console.log(sum)
-    res.status(200)
-    res.json({result:sum})
+    calculatorController.addNumbers(req, res)
     })
     
 router.get('/substract', (req, res) => {
-    let number1 = parseInt(req.query.num1);
-    let number2 = parseInt(req.query.num2);
-    let difference = number1 - number2
-    console.log(difference)
-    res.status(200)
-    res.json({result:difference})
+    calculatorController.substractNumbers(req, res)
     })
     
 router.get('/multiply', (req, res) => {
-    let number1 = parseInt(req.query.num1);
-    let number2 = parseInt(req.query.num2);
-    let sum = number1 * number2
-    console.log(product)
-    res.status(200)
-    res.json({result:product})
+    calculatorController.multiplyNumbers(req, res)
     })
 
 router.get('/divide', (req, res) => {
-    let number1 = parseInt(req.query.num1);
-    let number2 = parseInt(req.query.num2);
-    let sum = number1 / number2
-    console.log(fraction)
-    res.status(200)
-    res.json({result:fraction})
+    calculatorController.divideNumbers(req, res)
     })
 
 module.exports = router;
